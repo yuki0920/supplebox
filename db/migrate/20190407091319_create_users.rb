6fc08e576fc3c7224fcac00781c7927a6cd2a07a
yw0920 <ywsep20@gmail.com>
2019-04-07 09:16:10 +0000

    :sparkles: usersテーブルを追加#4

---
+++ db/migrate/20190407091319_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :nickname
+      t.string :weight
+      t.string :height
+      t.string :gender
+      t.string :comment
+
+      t.timestamps
+    end
+  end
+end
