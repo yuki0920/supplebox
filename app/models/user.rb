b95e57286e0d4e10a971e095f5d9e1079b9cac86
yw0920 <ywsep20@gmail.com>
2019-04-07 22:17:34 +0000

    :sparkles: Userモデルのnameカラムとemailカラムにバリデーション追加#4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  before_save { self.email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
   has_secure_password  
 end
