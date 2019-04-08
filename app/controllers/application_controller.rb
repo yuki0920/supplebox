c29635c9a293d03f2be04262c70b61d8840aa36f
yw0920 <ywsep20@gmail.com>
2019-04-08 22:14:59 +0000

    :bug: ログイン時のルーティングを修正#10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  include SessionsHelper
+
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   
   get 'login', to: 'sessions#new'
-  get 'login', to: 'sessions#create'
+  post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
