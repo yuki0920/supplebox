4a3700733e4a7f0c6f94bf5a9a45f90b8d40d24d
yw0920 <ywsep20@gmail.com>
2019-04-08 21:50:52 +0000

    :sparkles: ログイン/ログアウト用ルーティング設定#10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :create, :index]
