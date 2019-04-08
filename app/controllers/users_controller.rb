ac04da982c83d50908b91b8729cbf216a741374f
yw0920 <ywsep20@gmail.com>
2019-04-08 11:52:37 +0000

    :sparkles: usersコントローラーにnew/createアクションを追加#4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,25 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
   end
   
   def index
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :gender, :height, :weight, :comment)
   end
 end
