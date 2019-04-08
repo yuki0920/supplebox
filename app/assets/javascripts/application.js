4063c65c3a42917551b63161f5f7dd7afb4b22f2
yw0920 <ywsep20@gmail.com>
2019-04-08 22:29:13 +0000

    :art: jqueryを使えるよう設定を追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
