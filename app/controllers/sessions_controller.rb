eacb7aec3110c459d2e28b72aab1f138d9a20ab4
yw0920 <ywsep20@gmail.com>
2019-04-08 22:10:00 +0000

    :sparkles: ログイン/ログアウト用コントローラ設定#10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,34 @@ class SessionsController < ApplicationController
   end
 
   def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    if login(email, password)
+      flash[:success] = 'ログインに成功しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ログインに失敗しました。'
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
+  end
+
+  private
+
+  def login(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      # ログイン成功
+      session[:user_id] = @user.id
+      return true
+    else
+      # ログイン失敗
+      return false
+    end
   end
 end
