4d440e62ef6de5da75b61b71333943c7481f2e7e
yw0920 <ywsep20@gmail.com>
2019-04-13 09:15:27 +0000

    :sparkles: productクラスを作成し基本設定を追加#5

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,13 @@
 class Product
   # プロパティの設定
-  attr_accessor :title, :image_url, :url , :asin
+  attr_accessor :title, :image_url, :url , :asin, :brand, :price
 
-  def initialize(title, image_url, url, asin)
+  def initialize(title, image_url, url, asin, brand, price)
     @title = title
     @image_url = image_url
     @url = url
     @asin = asin
+    @brand = brand
+    @price = price
   end
 end
