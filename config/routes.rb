0d09e08a5b96f092234ef9a59325bea5441e59b4
yw0920 <ywsep20@gmail.com>
2019-04-14 03:14:50 +0000

    :sparkles: productsコント〜ローラーのアクションに対するルート設定#12

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :create, :index]
-  resources :products, only: [:new]
+  resources :products
 end
