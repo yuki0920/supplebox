b93d7d08f6021e6d18a1ab943db1b653fe485ea3
yw0920 <ywsep20@gmail.com>
2019-04-14 02:27:29 +0000

    :sparkles: productsテーブル追加に伴いプロパティ削除#12

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,2 @@
-class Product
-  # プロパティの設定
-  attr_accessor :title, :image_url, :url , :asin, :brand, :price
-
-  def initialize(title, image_url, url, asin, brand, price)
-    @title = title
-    @image_url = image_url
-    @url = url
-    @asin = asin
-    @brand = brand
-    @price = price
-  end
+class Product < ApplicationRecord
 end
