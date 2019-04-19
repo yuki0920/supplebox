59caa9cbb109e706f1e3aa5aa20dd477fa5b47cb
yw0920 <ywsep20@gmail.com>
2019-04-19 23:46:07 +0000

    :sparkles: :sparkles: 画像に対するバリデーションを追加#9

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :product
   mount_uploader :picture, PictureUploader
+  validate  :picture_size
+
+  private
+
+    # アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "ファイルサイズを5MBより小さくしてください")
+      end
+    end  
 end
