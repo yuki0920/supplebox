26f6ba4082be8f3b1fa5656307b571f065621adb
yw0920 <ywsep20@gmail.com>
2019-04-19 11:19:16 +0000

    :bug: 商品検索時にASINのみをfind_or_initialize_byで探索するよう変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,14 +16,13 @@ class ProductsController < ApplicationController
       # 商品のタイトル,画像URL, 詳細ページURLの取得
       @products = []
       products.items.each do |item|
-        product = Product.find_or_initialize_by(
-          title: item.get('ItemAttributes/Title'), #商品タイトル
-          image_url: item.get('LargeImage/URL'), #商品画像URL
-          url: item.get('DetailPageURL'), #商品詳細URL
-          asin: item.get('ASIN'), #商品にユニークなコード
-          brand_amazon_name: item.get('ItemAttributes/Brand'), #ブランド(メーカー)
-          price: item.get('OfferSummary/LowestNewPrice/Amount'), #実売価格を¥表示
-        )
+        product = Product.find_or_initialize_by(asin: item.get('ASIN')) #商品にユニークなコードで探索
+        product.title = item.get('ItemAttributes/Title'), #商品タイトル
+        product.image_url = item.get('LargeImage/URL'), #商品画像URL
+        product.url =  item.get('DetailPageURL'), #商品詳細URL
+        product.brand_amazon_name = item.get('ItemAttributes/Brand'), #ブランド(メーカー)
+        product.price = item.get('OfferSummary/LowestNewPrice/Amount'), #実売価格を¥表示
+
         @products << product
       end
     end
