78d32a34404b632dc75e33435009e0f8a4b6f5bc
yw0920 <ywsep20@gmail.com>
2019-04-20 02:30:37 +0000

    :sparkles: Productモデルにバリデーションを追加#20

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
   validates :asin, uniqueness: true
   has_many :posts
+
+  #お気に入り機能追加用中間テーブル追加
+  has_many :likes, foreign_key: 'product_id', dependent: :destroy
+  has_many :users, through: :likes  
 end
