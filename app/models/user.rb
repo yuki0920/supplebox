ffaaa543332a31d71f682ff49f32d0770ec42395
yw0920 <ywsep20@gmail.com>
2019-04-20 02:30:19 +0000

    :sparkles: Userモデルにバリデーションを追加#20

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,24 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :posts
+  
+  #お気に入り機能追加用中間テーブル追加
+  has_many :likes
+  has_many :products, through: :likes
+  
+  # お気に入り追加
+  def like(product)
+    likes.find_or_create_by(product_id: product.id)
+  end
+  
+  # お気に入り削除
+  def unlike(product)
+    like = likes.find_by(product_id: product.id)
+    like.destroy if like
+  end
+  
+  # お気に入り登録判定
+  def like?(product)
+    self.products.include?(product)
+  end
 end
