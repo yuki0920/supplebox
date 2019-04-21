889373a308cf10b2d1e9c2318cd0a42a0d8dfc22
yw0920 <ywsep20@gmail.com>
2019-04-21 02:40:42 +0000

    :sparkles: ユーザー詳細ページを更新できるようルーティングを追加#22

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:show, :create, :index] do
+  resources :users, only: [:show, :create, :index, :edit, :update] do
     member do
       get :followings
       get :followers
