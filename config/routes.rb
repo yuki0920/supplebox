f0580f3a1d6756f35d0f9ff17e8ddfc8d81fb97f
yw0920 <ywsep20@gmail.com>
2019-04-22 11:08:31 +0000

    :sparkles: お気に入り商品ランキング用ルーティングを追加#27

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
   get 'signup', to: 'users#new'
+  get 'rankings/like', to: 'rankings#like'
+ 
   resources :users, only: [:show, :create, :index, :edit, :update] do
     member do
       get :followings
