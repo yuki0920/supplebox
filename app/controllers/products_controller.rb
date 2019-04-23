8c3947a52fbad35afa4ed573374bae86f180809f
yw0920 <ywsep20@gmail.com>
2019-04-23 22:34:21 +0000

    :sparkles: 検索を利用できるようコントローラーを修正#41

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -70,6 +70,8 @@ class ProductsController < ApplicationController
   end
   
   def index
-    @products = Product.all.page(params[:page]).per(12)
+    # @products = Product.all.page(params[:page]).per(12)
+    @q = Product.all.ransack(params[:q])
+    @products = @q.result(distinct: true).page(params[:page]).per(12)
   end
 end
