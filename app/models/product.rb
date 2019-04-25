597a841ea01bfd32ea8c537ff3b86f4f95d0dafb
yw0920 <ywsep20@gmail.com>
2019-04-25 22:41:26 +0000

    :sparkles: Postモデルのバリデーションを追加#24

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,15 @@
 class Product < ApplicationRecord
-  validates :asin, uniqueness: true
+  
+  # 商品登録用
+  validates :title, presence: true, length: { maximum: 255 }
+  ## urlは255字以上のためデータ型が'text
+  validates :url, presence: true
+  validates :image_url, presence: true, length: { maximum: 255 }
+  ## ASINコードで一意に識別
+  validates :asin, uniqueness: true, length: { maximum: 255 }
+  
+  
+  # 口コミ投稿との関連付け
   has_many :posts
 
   #お気に入り機能追加用中間テーブル追加
