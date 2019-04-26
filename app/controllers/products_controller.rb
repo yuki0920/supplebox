ebec346ea8c395c8c4cde5ab381b961dd98c91bd
yw0920 <ywsep20@gmail.com>
2019-04-26 23:09:20 +0000

    商品登録機能の一部をログインユーザーのみに限定#69

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :require_user_logged_in, only: [:new, :create, :edit, :update, :destroy]
+  
   def new
     if params[:keyword].present?
       #　デバックログ出力するために記述
