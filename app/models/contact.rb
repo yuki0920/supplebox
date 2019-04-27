16e8af5fe9030492e9eb99395be29f03dd6307a2
yw0920 <ywsep20@gmail.com>
2019-04-27 02:34:07 +0000

    :sparkles: お問い合わせフォームにバリデーション追加#54

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,8 @@
 class Contact < ApplicationRecord
+  # ユーザーとの関連付け
   belongs_to :user
+  
+  # バリデーション
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true
 end
