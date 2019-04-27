490e9269e47a990514accf7becdc5549ab27743f
yw0920 <ywsep20@gmail.com>
2019-04-27 07:38:20 +0000

    :sparkles: トップページにお気に入り商品と最新の口コミ表示機能を追加#49

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,7 @@
 class ToppagesController < ApplicationController
   def index
+    @posts = Post.all.order(created_at: :desc).limit(4)
+    @ranking_counts = Like.ranking_top
+    @products = Product.find(@ranking_counts.keys)
   end
 end
