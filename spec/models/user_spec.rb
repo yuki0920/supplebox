290cf9dacbb4ed649f3c6c0ced156f67295c4902
yw0920 <ywsep20@gmail.com>
2019-04-30 23:46:24 +0000

    :sparkles: ユーザー新規登録時のnilのテスト追加#86

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
   end
   
+  it 'ユーザー名が無いと新規登録できないこと' do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include('を入力してください')
+  end
+  
+  it 'ニックネームが無いと新規登録できないこと' do
+    user = User.new(nickname: nil)
+    user.valid?
+    expect(user.errors[:nickname]).to include('を入力してください')
+  end
+ 
+  it 'メールアドレスが無いと新規登録できないこと' do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include('を入力してください')
+  end
+  
 end
