2f83163be06792b5890ae3176576cf602c5b6394
yw0920 <ywsep20@gmail.com>
2019-04-30 03:16:53 +0000

    :sparkles: ユーザー詳細表示のテスト追加#82

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -6,9 +6,23 @@ describe 'ユーザー表示機能', type: :system do
       user_a = FactoryBot.create(:user, name: 'UserA', nickname: 'ユーザーA', email: 'a@example.com')
     end
     
-    context 'ユーザーAがログインしていとき' do
+    context 'ログイン状態に関わらず' do
       before do
         visit users_path
+      end
+      
+      it 'ユーザーAが表示される' do
+        expect(page).to have_content 'UserA'
+      end
+    end
+  end
+  
+  describe '詳細表示機能' do
+    context 'ログイン状態に関わらず' do
+      
+      before do
+        user_a = FactoryBot.create(:user, name: 'UserA', nickname: 'ユーザーA', email: 'a@example.com')
+        visit user_path(user_a)
       end
       
       it 'ユーザーAが表示される' do
