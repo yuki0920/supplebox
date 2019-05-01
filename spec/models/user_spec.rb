2f57a0650c34016d9c614931873143447759df27
yw0920 <ywsep20@gmail.com>
2019-05-01 07:53:33 +0000

    :bug: ユーザー新規登録のテストを修正#86

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,40 +2,31 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it '新規登録できること' do
-      expect(FactoryBot.build(:user)).to be_valid
+    user = FactoryBot.build(:user)
+    expect(user).to be_valid
   end
   
   it 'ユーザー名が無いと新規登録できないこと' do
-    user = User.new(name: nil)
+    user = FactoryBot.build(:user, name: nil)
     user.valid?
     expect(user.errors[:name]).to include('を入力してください')
   end
   
   it 'ニックネームが無いと新規登録できないこと' do
-    user = User.new(nickname: nil)
+    user = FactoryBot.build(:user, nickname: nil)
     user.valid?
     expect(user.errors[:nickname]).to include('を入力してください')
   end
  
   it 'メールアドレスが無いと新規登録できないこと' do
-    user = User.new(email: nil)
+    user = FactoryBot.build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include('を入力してください')
   end
   
   it '重複したメールアドレスなら無効な状態であること' do
-    User.create(
-      name: 'Test',
-      nickname: 'テスト',
-      email: 'tester@supplebox.jp',
-      password: 'tester',            
-      )
-    user = User.new(
-      name: 'Sample',
-      nickname: 'テスト',
-      email: 'tester@supplebox.jp',
-      password: 'tester',            
-      )
+    FactoryBot.create(:user)
+    user = FactoryBot.build(:user)
     user.valid?
     expect(user.errors[:email]).to include('はすでに存在します')
   end
