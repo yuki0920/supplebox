1ba8d9d90e42c8e8b70e3aa7420de9cec9680957
yw0920 <ywsep20@gmail.com>
2019-05-02 10:13:18 +0000

    :sparkles: 商品モデルのテストをリファクタリング#89

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,41 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  let(:product) { FactoryBot.build(:product) }
+  let(:other_product) { FactoryBot.build(:product) }
+  
   it '商品登録できること' do
-    product = FactoryBot.create(:product)
-    product.valid?
     expect(product).to be_valid
   end
   
   it 'アイテム名称がないと無効な状態であること' do
-    product = FactoryBot.build(:product, title: nil)
+    product.title = nil
     product.valid?
     expect(product.errors[:title]).to include('を入力してください')
   end
   
     it 'URLがないと無効な状態であること' do
-    product = FactoryBot.build(:product, url: nil)
+    product.url = nil
     product.valid?
     expect(product.errors[:url]).to include('を入力してください')
   end
   
   it '画像URLがないと無効な状態であること' do
-    product = FactoryBot.build(:product, image_url: nil)
+    product.image_url = nil
     product.valid?
     expect(product.errors[:image_url]).to include('を入力してください')
   end
 
     it 'アイテムコードがないと無効な状態であること' do
-    product = FactoryBot.build(:product, asin: nil)
+    product.asin = nil
     product.valid?
     expect(product.errors[:asin]).to include('を入力してください')
   end
   
   it '重複したアイテムコードなら無効な状態であること' do
-    FactoryBot.create(:product, asin: 'TESTPRODUCT1')
-    product = FactoryBot.build(:product, asin: 'TESTPRODUCT1')
-    product.valid?
-    expect(product.errors[:asin]).to include('はすでに存在します')
+    product.asin = 'TESTPRODUCT1'
+    product.save
+    other_product.asin = 'TESTPRODUCT1'
+    other_product.valid?
+    expect(other_product.errors[:asin]).to include('はすでに存在します')
   end
 
 end
