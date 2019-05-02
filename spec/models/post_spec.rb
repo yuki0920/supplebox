75d5fa4ca51d5013f4e831d2b9f3fc4917a776d0
yw0920 <ywsep20@gmail.com>
2019-05-02 10:14:35 +0000

    :sparkles: 口コミ投稿モデルのテストをリファクタリング&同一ユーザー同一アイテムの口コミに対するユニーク制約を追加#89

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,62 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-
-  it '口コミ投稿できること' do
-    post = FactoryBot.create(:post)
-    expect(post).to be_valid
+  let(:post) { FactoryBot.create(:post) }
+  let(:other_post) { FactoryBot.create(:post) }  
+  
+  describe '口コミ投稿が有効な場合' do
+    it '口コミ投稿ができること' do
+      expect(post).to be_valid
+    end
   end
 
   describe '口コミタイトルを検証する場合' do
-    
     it 'タイトルがないと無効な状態であること' do
-     post = FactoryBot.build(:post, title: nil)
+     post.title = nil
      post.valid?
      expect(post.errors[:title]).to include('を入力してください')
     end
-    
   end
   
   describe '口コミ評価を検証する場合' do
     
     it '口コミ評価がないと無効な状態であること' do
-     post = FactoryBot.build(:post, rate: nil)
+     post.rate = nil
      post.valid?
      expect(post.errors[:rate]).to include('を入力してください')
     end
     
     it '口コミ評価が整数でないと無効な状態であること' do
-      post = FactoryBot.build(:post, rate: '1.5')
+      post.rate = '1.5'
       post.valid?
       expect(post.errors[:rate]).to include('は整数で入力してください')
     end
     
     it '口コミ評価が1未満だと無効な状態であること' do
-      post = FactoryBot.build(:post, rate: '-1')
+      post.rate = '-1'
       post.valid?
       expect(post.errors[:rate]).to include('は1以上の値にしてください')
     end
     
     it '口コミ評価が5超だと無効な状態であること' do
-      post = FactoryBot.build(:post, rate: '6')
+      post.rate = '6'
       post.valid?
       expect(post.errors[:rate]).to include('は5以下の値にしてください')
     end
-    
   end
   
   describe '口コミ内容を検証する場合' do
-    
     it '口コミ内容がないと無効な状態であること' do
-     post = FactoryBot.build(:post, content: nil)
+     post.content = nil
      post.valid?
      expect(post.errors[:content]).to include('を入力してください')
     end
-    
   end
   
-  
-    
-
+  describe 'ユーザーを検証する場合' do
+    it '1つのアイテムに対して口コミが重複すると無効な状態であること' do
+      other_post.product_id = post.product_id
+      other_post.user_id = post.user_id
+      other_post.valid?
+      expect(other_post.errors[:user_id]).to include('はすでに存在します')
+    end
+  end
 
 end
