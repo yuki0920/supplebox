df0d848838ac867c861c5d2e50c96c238642e71d
yw0920 <ywsep20@gmail.com>
2019-05-02 22:38:03 +0000

    :sparkles: ユーザー新規登録・編集の統合テスト追加#91

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,6 +1,38 @@
 require 'rails_helper'
 
 describe 'ユーザー表示機能', type: :system do
+
+  it '新しいユーザーを作成できること' do
+    expect {
+      visit root_path
+      click_link '新規登録'
+      fill_in 'ユーザー名', with: 'TestUser'
+      fill_in 'ニックネーム', with: 'テストユーザー'
+      fill_in 'メールアドレス', with: 'tester@supplebox.jp'
+      fill_in 'パスワード', with: 'password'
+      fill_in 'パスワード（確認）', with: 'password'
+      click_on '登録する'
+    }.to change{ User.count }.by(+1)
+    expect(page).to have_content 'ユーザを登録しました。'
+    expect(page).to have_content 'テストユーザー'
+  end
+
+  it 'プロフィールを編集出来ること' do
+    # sign_in_as user
+    visit root_path
+    click_link '新規登録'
+    fill_in 'ユーザー名', with: 'TestUser'
+    fill_in 'ニックネーム', with: 'テストユーザー'
+    fill_in 'メールアドレス', with: 'tester@supplebox.jp'
+    fill_in 'パスワード', with: 'password'
+    fill_in 'パスワード（確認）', with: 'password'
+    click_on '登録する'
+    click_on 'プロフィールを編集する'
+    fill_in '自己紹介', with: 'こんにちは.'
+    click_on '更新する'
+    expect(page).to have_content 'プロフィールを更新しました'
+  end  
+  
   describe '一覧表示機能' do
     before do
       user_a = FactoryBot.create(:user, name: 'UserA', nickname: 'ユーザーA', email: 'a@example.com')
