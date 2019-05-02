7369c64d28dc8deb2966f3eead9d351df3f94307
yw0920 <ywsep20@gmail.com>
2019-05-02 01:37:28 +0000

    :sparkles: ユーザー登録のモデルテストをリファクタリング#89

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,129 +1,123 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { FactoryBot.create(:user) }
+  let(:other_user) { FactoryBot.create(:user) }
+  let(:product) { FactoryBot.create(:product) }
+  let(:other_product) { FactoryBot.create(:product) }
 
   describe  'ユーザー登録できる場合' do
-    
     it '新規登録できること' do
-      user = FactoryBot.build(:user)
       expect(user).to be_valid
     end
   end
   
   describe 'ユーザー名を検証する場合' do
-
-    it 'ユーザー名が無いと新規登録できないこと' do
-      user = FactoryBot.build(:user, name: nil)
+    it 'ユーザー名が無いと無効な状態であること' do
+      user.name = nil
       user.valid?
       expect(user.errors[:name]).to include('を入力してください')
     end
 
     it 'ユーザー名が50文字超なら無効な状態であること' do
-      user = FactoryBot.build(:user, name: 'a' * 51 )
+      user.name = 'a' * 51
       user.valid?
       expect(user.errors[:name]).to include('は50文字以内で入力してください')
     end
-
   end
   
   describe 'ニックネームを検証する場合' do
-    it 'ニックネームが無いと新規登録できないこと' do
-      user = FactoryBot.build(:user, nickname: nil)
+    it 'ニックネームが無いと無効な状態であること' do
+      user.nickname = nil
       user.valid?
       expect(user.errors[:nickname]).to include('を入力してください')
     end
   
     it 'ニックネームが50文字超なら無効な状態であること' do
-      user = FactoryBot.build(:user, nickname: 'a' * 51 )
+      user.nickname = 'a' * 51
       user.valid?
       expect(user.errors[:nickname]).to include('は50文字以内で入力してください')
     end
   end
 
   describe 'メールアドレスを検証する場合' do
-    it 'メールアドレスが無いと新規登録できないこと' do
-      user = FactoryBot.build(:user, email: nil)
+    it 'メールアドレスが無いと無効な状態であること' do
+      user.email = nil
       user.valid?
       expect(user.errors[:email]).to include('を入力してください')
     end
     
     it '重複したメールアドレスなら無効な状態であること' do
       FactoryBot.create(:user, email: 'tester@supplebox.jp')
-      user = FactoryBot.build(:user, email: 'tester@supplebox.jp')
+      user.email = 'tester@supplebox.jp'
       user.valid?
       expect(user.errors[:email]).to include('はすでに存在します')
     end
     
     it 'メールアドレスに@が含まれていないなら無効な状態であること' do
-      user = FactoryBot.build(:user, email: 'supplebox.jp' )
+      user.email = 'supplebox.jp'
       user.valid?
       expect(user.errors[:email]).to include('は不正な値です')
     end
     
     it 'メールアドレスが保存される前に小文字に変換されること' do
-      user = FactoryBot.build(:user, email: 'TESTADD@supplebox.jp')
+      user.email = 'TESTADD@supplebox.jp'
       user.save
       expect(user.email).to eq 'testadd@supplebox.jp'
     end
-    
   end
   
   describe 'follow/unfollow/following?メソッドを検証する場合' do
-    let(:user) { FactoryBot.create(:user) }
-    let(:other_user) { FactoryBot.create(:user) }
-    
-    it 'followしていないとfollow状態にならないこと' do
-      expect(user.following?(other_user)).to eq false
-    end
-
-    it 'followしているとfollow状態になること' do
-      user.follow(other_user)
-      expect(other_user.followers).to include user
-      expect(user.following?(other_user)).to eq true
-    end
-
-    it 'followしてunfollowするとfollow状態にならないこと' do
-      user.follow(other_user)
-      user.unfollow(other_user)
-      expect(user.following?(other_user)).to eq false
+    context 'followしていない状態の場合' do
+      it '無効な状態であること' do
+        expect(user.following?(other_user)).to eq false
+      end
     end
     
+    context 'followした場合' do
+      before do
+        user.follow(other_user)
+      end
+         
+      it 'follow状態になること' do
+        expect(other_user.followers).to include user
+        expect(user.following?(other_user)).to eq true
+      end
+  
+      it 'unfollowすると無効な状態であること' do
+        user.unfollow(other_user)
+        expect(user.following?(other_user)).to eq false
+      end
+    end
   end
   
   describe 'like/unlike/like?メソッドを検証する場合' do
-    let(:user) { FactoryBot.create(:user) }
-    let(:other_user) { FactoryBot.create(:user) }
-    let(:product) { FactoryBot.create(:product) }
-    let(:other_product) { FactoryBot.create(:product) }
-    
+
     context 'likeしていない状態の場合' do
-      it '有効でないこと' do
+      it '無効な状態であること' do
         expect(user.like?(product)).to eq false
       end
     end
     
-    context 'likeしている状態の場合' do
+    context 'likeした場合' do
       before do
         user.like(product)
       end
       
-      it 'likeしたアイテムが有効であること' do
+      it 'like状態であること' do
         expect(user.like?(product)).to eq true
       end
       
-      it 'likeしていないアイテムが有効でないこと' do
+      it 'likeしていないアイテムが無効な状態であること' do
         expect(user.like?(other_product)).to eq false
         expect(other_user.like?(other_product)).to eq false
         expect(other_user.like?(product)).to eq false
       end
       
-      it 'unlikeするとアイテムが有効でないこと' do
+      it 'unlikeすると無効な状態であること' do
         user.unlike(product)
         expect(user.like?(product)).to eq false
       end
-      
     end
-
   end
-  
 end
