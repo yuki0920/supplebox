34dc87d4a7c1ece17423e13d1a6ac5a1980ad6e9
yw0920 <ywsep20@gmail.com>
2019-05-03 13:02:16 +0000

    :sparkles: herokuドメインから独自ドメインへのリダイレクト設定を追加#93

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_filter :ensure_domain
   protect_from_forgery with: :exception
 
   include SessionsHelper
 
   private
+  
+  # redirect correct server from herokuapp domain for SEO
+  def ensure_domain
+   return unless /\.herokuapp.com/ =~ request.host
+  
+   # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める 
+   port = ":#{request.port}" unless [80, 443].include?(request.port)
+   redirect_to "#{request.protocol}#{supplebox.jp}#{port}#{request.path}", status: :moved_permanently
+  end  
 
   def require_user_logged_in
     unless logged_in?
