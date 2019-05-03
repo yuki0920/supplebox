925ea34f0af82b63ca0683ab18eb754412862058
yw0920 <ywsep20@gmail.com>
2019-05-03 09:25:24 +0000

    :sparkles: アイテム検索機能とアイテム登録機能のテストを追加#91

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -10,5 +10,14 @@ describe 'アイテム登録機能', type: :system do
     click_on '商品を検索'
     expect(page).to have_content 'プロテイン'
   end
-  
+
+  it '登録したアイテム詳細画面を確認できること' do
+    user = FactoryBot.create(:user)
+    sign_in_as user
+    product = FactoryBot.create(:product)
+    visit products_path
+    click_link product.title
+    expect(page).to have_content('Amazonで詳しく見る')
+  end
+
 end
