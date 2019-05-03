5795c3ec91fb2956656b4c4f919ccab65c8c66a0
yw0920 <ywsep20@gmail.com>
2019-05-03 00:23:26 +0000

    :sparkles: 他のユーザーのプロフィール制限機能のテストを追加#91

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'ユーザー表示機能', type: :system do
   let!(:user) { FactoryBot.create(:user) }
-  
+  let!(:other_user) { FactoryBot.create(:user) }  
   it '新しいユーザーを作成できること' do
     expect {
       visit root_path
@@ -19,12 +19,20 @@ describe 'ユーザー表示機能', type: :system do
   end
 
   it 'プロフィールを編集出来ること' do
+    visit edit_user_path(user)
     sign_in_as user
     click_on 'プロフィールを編集する'
     fill_in '自己紹介', with: 'こんにちは.'
     click_on '更新する'
     expect(page).to have_content 'プロフィールを更新しました'
-  end  
+  end
+  
+  it "他のユーザーの編集ページにはアクセスできない" do
+    sign_in_as user
+    visit edit_user_path(other_user)
+    expect(page).not_to have_current_path edit_user_path(other_user)
+    expect(page).to have_current_path user_path(user)
+  end
   
   describe '一覧表示機能' do
     before do
