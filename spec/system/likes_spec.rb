e2be89440a8b05630c515a3cc927c27f30995998
yw0920 <ywsep20@gmail.com>
2019-05-04 01:03:43 +0000

    :sparkles: お気に入り登録機能の統合テストを追加#91

---
+++ spec/system/likes_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe 'アイテムお気に入り登録機能' do
+
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:product){ FactoryBot.create(:product, title: 'お気に入りアイテム')}
+  
+  context 'お気に入り登録していない場合' do
+    it 'お気に入り登録できる' do
+      sign_in_as user
+      visit product_path(product)
+      expect{
+        click_on 'お気に入り登録'}.to change{ Like.count}.by(+1)
+      expect(page).to have_content 'お気に入り登録をしました。'
+    end
+  end
+  
+  context 'お気に入り登録している場合' do
+    before do
+      sign_in_as user
+      visit product_path(product)
+      click_on 'お気に入り登録'
+    end
+    
+    it 'お気に入りしたアイテムがランキングに表示されている' do
+      visit rankings_like_path
+      expect(page).to have_content product.title
+    end
+    
+    it 'お気に入りしたアイテムがユーザープロフィールに表示されている' do
+      visit like_products_user_path(user)
+      expect(page).to have_content product.title
+    end
+    
+    it 'お気に入り登録を解除できる' do
+      expect{
+        click_on 'お気に入り登録中'}.to change{ Like.count}.by(-1)
+      expect(page).to have_content 'お気に入り登録を解除しました。'
+    end
+    
+  end
+  
+end
