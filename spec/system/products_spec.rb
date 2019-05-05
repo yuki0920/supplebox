64daf8fa5fac3578ed1cc2beea6ef623a4d4834a
yw0920 <ywsep20@gmail.com>
2019-05-05 02:09:01 +0000

    :sparkles: 商品削除機能を管理者のみに制限する機能のテストを追加#35

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe 'アイテム登録機能', type: :system do
   let(:user) { FactoryBot.create(:user) }
+  let(:admin_user) { FactoryBot.create(:user, admin: true) }
   let!(:product) { FactoryBot.create(:product) }
   
   # it 'アイテムを検索できること' do
@@ -16,6 +17,19 @@ describe 'アイテム登録機能', type: :system do
     visit products_path
     click_link product.title
     expect(page).to have_content('Amazonで詳しく見る')
+  end
+  
+  it '管理者は商品を削除できること' do
+    sign_in_as admin_user
+    visit products_path
+    expect{click_on '商品削除'}.to change{ Product.count}.by(-1)
+    expect(page).to have_content '商品を削除しました'
+  end
+  
+  it '一般ユーザーは商品を削除できないこと' do
+    sign_in_as user
+    visit products_path
+    expect(page).to_not have_content '商品削除'
   end
 
 end
