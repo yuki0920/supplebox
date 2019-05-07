9603efa4a5b19e65d8b7841c32fc88f583b63d25
yw0920 <ywsep20@gmail.com>
2019-05-07 09:53:30 +0000

    :sparkles: ユーザーフォローボタンに非同期通信 機能を追加&ユーザー一覧のフォローボタンを無効化#29

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,9 @@ module Supplebox
         helper_specs: false,
         routing_specs: false
     end    
-      
+
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+    
   end
 end
