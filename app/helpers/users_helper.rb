ed82fcb723759d129e9e916e03aa2855f57d39fe
yw0920 <ywsep20@gmail.com>
2019-05-09 14:03:05 +0000

    :sparkles: グラバターの設定ファイルを削除

--- app/helpers/users_helper.rb
+++
@@ -1,8 +0,0 @@
-module UsersHelper
-  def gravatar_url(user, options = { size: 80 })
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    #[retro]はデフォルトの画像を設定
-    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=retro"
-  end
-end
