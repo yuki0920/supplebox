7c8d8dd24b8fc716f0a026be84b0d02de85e980c
yw0920 <ywsep20@gmail.com>
2019-05-10 11:53:48 +0000

    :sparkles: アイテム登録機能の統合テストを整理

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -5,31 +5,38 @@ describe 'アイテム登録機能', type: :system do
   let(:admin_user) { FactoryBot.create(:user, admin: true) }
   let!(:product) { FactoryBot.create(:product) }
   
-  # it 'アイテムを検索できること' do
-  #   sign_in_as user
-  #   visit new_product_path
-  #   fill_in 'keyword', with: 'プロテイン'
-  #   click_on '商品を検索'
-  #   expect(page).to have_content 'プロテイン'
+  # describe '検索機能' do
+    # it 'アイテムを検索できること' do
+    #   sign_in_as user
+    #   visit new_product_path
+    #   fill_in 'keyword', with: 'プロテイン'
+    #   click_on '商品を検索'
+    #   expect(page).to have_content 'プロテイン'
+    # end
   # end
-
-  it 'ログイン状態に関わらず登録したアイテム詳細画面を確認できること' do
-    visit products_path
-    click_link product.title
-    expect(page).to have_content('Amazonで詳しく見る')
+  
+  describe '詳細表示機能' do
+    it 'ログイン状態に関わらず登録したアイテム詳細画面を確認できること' do
+      visit products_path
+      click_link product.title
+      expect(page).to have_content('Amazonで詳しく見る')
+    end
   end
   
-  it '管理者は商品を削除できること' do
-    sign_in_as admin_user
-    visit products_path
-    expect{click_on '商品削除'}.to change{ Product.count}.by(-1)
-    expect(page).to have_content '商品を削除しました'
-  end
+  describe '削除機能' do
+    
+    it '管理者は商品を削除できること' do
+      sign_in_as admin_user
+      visit products_path
+      expect{click_on '商品削除'}.to change{ Product.count}.by(-1)
+      expect(page).to have_content '商品を削除しました'
+    end
   
-  it '一般ユーザーは商品を削除できないこと' do
-    sign_in_as user
-    visit products_path
-    expect(page).to_not have_content '商品削除'
+    it '一般ユーザーは商品を削除できないこと' do
+      sign_in_as user
+      visit products_path
+      expect(page).to_not have_content '商品削除'
+    end
   end
   
   describe '編集機能' do
@@ -51,5 +58,4 @@ describe 'アイテム登録機能', type: :system do
     end
   
   end
-
 end
