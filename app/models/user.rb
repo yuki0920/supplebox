98148c83af22ca794c878017c06fe3ff29b8e771
yw0920 <ywsep20@gmail.com>
2019-05-12 00:23:10 +0000

    :sparkles: ユーザーモデルのパスワードに関する検証とテストを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
   validates :comment, length: { maximum: 100 }
   mount_uploader :picture, AvatarUploader                    
   
