4295d2ebf87c2ece24c785ba5e39b6ef1b8c3931
yw0920 <ywsep20@gmail.com>
2019-05-19 12:27:48 +0000

    :bug: 口コミ評価カラムのマイグレーションファイルを環境ごとに設定#119

--- db/migrate/20190518124329_rename_rate_to_posts.rb
+++ db/migrate/20190518124329_rename_rate_to_posts.rb
@@ -1,6 +1,10 @@
 class RenameRateToPosts < ActiveRecord::Migration[5.2]
-  def up  
-    change_column :posts, :rate, :float
+  def up
+    if Rails.env.development? || Rails.env.test?
+      change_column :posts, :rate, :float
+    else Rails.env.production?
+      change_column :posts, :rate, 'float USING CAST(rate AS float)'
+    end
   end
   
   def down
