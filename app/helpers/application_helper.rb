ae1f75e248add4390988a5d3e33be2490ca4964b
yw0920 <ywsep20@gmail.com>
2019-06-06 22:10:50 +0000

    :sparkles: meta-tags を追加#140

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,35 @@ module ApplicationHelper
     time.strftime("%Y/%m/%d %H:%M")
   end
   
+  # メタタグを表示するメソッド
+  def default_meta_tags
+    {
+      site: 'サイト名',
+      title: 'SuppleBox プロテイン口コミ・評判サイト',
+      reverse: true,
+      charset: 'utf-8',
+      description: '実際に購入した方のプロテインの口コミ・評価が閲覧できるサイトです。「プロテイン選びで失敗したくない」方は、当サイトで味や価格をチェックして、自分にあったプロテインを見つけてください。',
+      keywords: 'プロテイン,ホエイプロテイン,口コミ,評判,おいしい,選び方,',
+      canonical: request.original_url,
+      separator: '|',
+      # icon: [
+      #   { href: image_url('favicon.ico') },
+      #   { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      # ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        # image: image_url('ogp.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary',
+        site: '@YukiWebTech',
+      }
+    }
+  end  
+  
 end
