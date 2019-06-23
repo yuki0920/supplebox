826c2bbad3a022858d6de0b77914bb684588a157
yw0920 <ywsep20@gmail.com>
2019-06-23 12:26:06 +0000

    :bug: テストごとにymlが更新されるためapiのテストを一時無効化

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   before_save { self.email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   validates :nickname, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }, allow_nil: true
   validates :comment, length: { maximum: 100 }
