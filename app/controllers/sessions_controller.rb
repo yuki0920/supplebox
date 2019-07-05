b917e868bff59c7b28e692ddd519ee3d457389e6
yw0920 <ywsep20@gmail.com>
2019-07-05 13:25:29 +0000

    sparkles: フレンドリーフォワーディング機能を追加#160

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     password = params[:session][:password]
     if login(email, password)
       flash[:success] = 'ログインに成功しました。'
-      redirect_to @user
+      redirect_back_or @user
     else
       flash.now[:danger] = 'ログインに失敗しました。'
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,13 @@ module SessionsHelper
   def logged_in?
     !!current_user
   end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
