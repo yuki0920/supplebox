ee26401dd7f5f9debf4862a76830cf21bd4c327e
yuki0920 <ywsep20@gmail.com>
2019-10-31 14:33:12 +0900

    ✨ gem 'bullet'追加 #177

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,12 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.after_initialize do
+    Bullet.enable = true # Bulletプラグインを有効
+    Bullet.alert = true # JavaScriptでの通知
+    Bullet.bullet_logger = true # log/bullet.logへの出力
+    Bullet.console = true # ブラウザのコンソールログに記録
+    Bullet.rails_logger = true # Railsログに出力
+  end
 end
