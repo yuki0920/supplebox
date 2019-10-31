d36aec1548f54a0e48601f8835248f28fb62940f
yuki0920 <ywsep20@gmail.com>
2019-10-31 14:18:55 +0900

    ✨ シードデータ整備 #175

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,47 @@
-# User.create!(name:  "Admin User",
-#             email: "admin@supplebox.jp",
-#             password: "admin0",
-#             password_confirmation: "admin0",
-#             nickname: "管理者",
-#             admin: true)
-# 29.times do |n|
-#   name     = Faker::Name.name
-#   email    = Faker::Internet.email
-#   password = "password"
-#   User.create!(name: name,
-#             email: email,
-#             password: password,
-#             password_confirmation: password,
-#             nickname: name)
-# end
+# ユーザー登録
+User.create!(name:  "Admin User",
+            email: "admin@supplebox.jp",
+            password: "admin0",
+            password_confirmation: "admin0",
+            nickname: "管理者",
+            admin: true)
+29.times do |n|
+  name     = Faker::Name.name
+  email    = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+            email: email,
+            password: password,
+            password_confirmation: password,
+            nickname: name)
+end
 
-# 30.times do |n|
-#   Product.create!(title: "NO.#{n}プロテイン",
-#                   url: "sample#{n}@supplebox.jp",
-#                   image_url: "sample_image#{n}@supplebox.jp",
-#                   asin: "code#{n}")
-# end
+# アイテム登録
+30.times do |n|
+  Product.create!(title: "NO.#{n}プロテイン",
+                  url: "sample#{n}@supplebox.jp",
+                  image_url: "sample_image#{n}@supplebox.jp",
+                  asin: "code#{n}")
+end
 
-# users = User.order(:created_at).take(6)
-# (1..30).each do |n|
-#   title = Faker::Lorem.sentence(5)
-#   content = Faker::Lorem.sentence
-#   product_id = n.to_i
-#   users.each { |user| user.posts.create!(title: title,
-#                                         content: content,
-#                                         rate: [1, 2, 3, 4, 5].sample,
-#                                         product_id: product_id )}
-# end
+users = User.order(:created_at).take(6)
+(1..30).each do |n|
+  title = Faker::Lorem.sentence(5)
+  content = Faker::Lorem.sentence
+  product_id = n.to_i
+  users.each { |user| user.posts.create!(title: title,
+                                        content: content,
+                                        rate: [1, 2, 3, 4, 5].sample,
+                                        product_id: product_id )}
+end
 
 # リレーションシップ
-# users = User.all
-# user  = users.first
-# following = users[2..30]
-# followers = users[3..20]
-# following.each { |followed| user.follow(followed) }
-# followers.each { |follower| follower.follow(user) }
+users = User.all
+user  = users.first
+following = users[2..30]
+followers = users[3..20]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
 
 # お気に入り登録
 users = User.order(:created_at).take(6)
@@ -49,3 +51,13 @@ users.each do |user|
     user.like(product)
   end
 end
+
+# ブランド登録
+(1..5).each do
+  Brand.create!(
+    name: Faker::Coffee.blend_name,
+    content:Faker::Coffee.notes
+  )
+end
+products = Product.all
+products.each { |product| product.brand_id = [1, 2, 3, 4, 5].sample }
