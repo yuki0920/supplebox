b7e969140914209c4d5c8977976618d184ec301e
yuki0920 <ywsep20@gmail.com>
2019-10-31 12:47:47 +0900

    :sparkles: 口コミ投稿のシードデータ追加 #173

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,21 @@ User.create!(name:  "Admin User",
             password_confirmation: password,
             nickname: name)
 end
+
 30.times do |n|
   Product.create!(title: "NO.#{n}プロテイン",
                   url: "sample#{n}@supplebox.jp",
                   image_url: "sample_image#{n}@supplebox.jp",
                   asin: "code#{n}")
+end
+
+users = User.order(:created_at).take(6)
+(1..30).each do |n|
+  title = Faker::Lorem.sentence(5)
+  content = Faker::Lorem.sentence
+  product_id = n.to_i
+  users.each { |user| user.posts.create!(title: title,
+                                        content: content,
+                                        rate: [1, 2, 3, 4, 5].sample,
+                                        product_id: product_id )}
 end
