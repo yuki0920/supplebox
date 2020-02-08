c2146e5f4aab5bbf8582bbf3daf3da38c53409af
yuki0920 <ywsep20@gmail.com>
2020-02-08 10:45:46 +0900

    セッション管理のテストを追加した

---
+++ spec/system/sessions_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'セッション管理機能', type: :system do
+  let!(:user) {
+    create(:user,
+           email: 'test@supplebox.jp',
+           password: 'password')}
+
+  it '登録済みユーザーがログインとログアウトできること' do
+    visit login_path
+    fill_in 'メールアドレス', with: 'test@supplebox.jp'
+    fill_in 'パスワード', with: 'password'
+    within '.login' do
+      click_on 'ログイン'
+    end
+    expect(page).to have_content 'ログインに成功しました。'
+    click_on 'ログアウト'
+    expect(page).to have_content 'ログアウトしました。'
+  end
+
+  it '登録済みでないユーザーがログインできないこと' do
+    visit login_path
+    fill_in 'メールアドレス', with: 'test@supplebox.jp'
+    fill_in 'パスワード', with: 'dummy_password'
+    within '.login' do
+      click_on 'ログイン'
+    end
+    expect(page).to have_content 'ログインに失敗しました。'
+  end
+end
