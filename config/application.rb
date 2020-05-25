6a6b1d4a3e65127ef6b41bb2043ad36f7e0f7556
yuki0920 <ywsep20@gmail.com>
2020-05-25 20:59:47 +0900

    zeitwerkのオートローディングモードを有効にした

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Supplebox
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
     # 日本時間対応
     config.time_zone = 'Asia/Tokyo'
 
