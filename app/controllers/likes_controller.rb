261d148a147c42d507507703cb8cbd6ec111708a
yuki0920 <ywsep20@gmail.com>
2020-07-04 12:36:52 +0900

    お気に入り登録を非同期通信でできるようにした

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,10 +4,15 @@ class LikesController < ApplicationController
   before_action :require_user_logged_in, only: %i[create destroy]
 
   def create
-    product = Product.find(params[:product_id])
-    current_user.like(product)
-    flash[:success] = 'お気に入り登録をしました。'
-    redirect_back(fallback_location: root_path)
+    @product = Product.find(params[:product_id])
+    current_user.like(@product)
+    respond_to do |format|
+      format.html do
+        flash[:success] = 'お気に入り登録をしました。'
+        redirect_back(fallback_location: root_path)
+      end
+      format.js
+    end
   end
 
   def destroy
