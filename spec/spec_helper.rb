0dcea26abb5365b0d798841362ba23a0645a3b0a
yuki0920 <ywsep20@gmail.com>
2020-07-04 14:18:20 +0900

    失敗するテストを記録して `--only-failures`オプションで実行できるようにした

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,8 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.example_status_persistence_file_path = 'spec/tmp/examples'
 end
 
 # VCRの設定追加
