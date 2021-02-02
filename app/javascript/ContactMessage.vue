039256f3efe22cee6c4d4d52cf379c82436883e4
yuki0920 <ywsep20@gmail.com>
2021-02-02 22:14:33 +0900

    問い合わせフォーム仮実装

--- app/javascript/ContactMessage.vue
+++ app/javascript/ContactMessage.vue
@@ -1,14 +1,51 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <h1>お問い合わせ</h1>
+    <p>お気軽にお問い合わせください!</p>
+    <form>
+      <div class="form-group row">
+        <label for="contact_name" class="col-sm-3 col-form-label">お名前</label>
+        <div class="col-sm-9">
+          <input v-model="name" type="text" class="form-control" id="contact_name">
+        </div>
+        <!-- NOTE: 双方向バインディング検証用 -->
+        <p>名前: {{ name }}</p>
+      </div>
+      <div class="form-group row">
+        <label for="contact_email" class="col-sm-3 col-form-label">メールアドレス</label>
+        <div class="col-sm-9">
+          <input type="email" class="form-control" id="contact_email">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="contact_title" class="col-sm-3 col-form-label">タイトル</label>
+        <div class="col-sm-9">
+          <input type="text" class="form-control" id="contact_title">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="contact_content" class="col-sm-3 col-form-label">お問い合わせ内容</label>
+        <div class="col-sm-9">
+          <textarea class="form-control" id="contact_content" rows="3"></textarea>
+        </div>
+      </div>
+      <div class="form-group row justify-content-end">
+        <div class="col-sm-9">
+          <button type="submit" class="btn btn-success">送信する</button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
+  data() {
     return {
-      message: "お気軽にお問い合わせください!"
+      name: "",
+      email: "",
+      title: "",
+      content: ""
     }
   }
 }
