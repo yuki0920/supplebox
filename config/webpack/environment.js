12c86f9c23e28af886fd4ae3d4e81ea3279bb464
yuki0920 <ywsep20@gmail.com>
2021-02-07 07:29:04 +0900

    Add webpack settings

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,16 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
+
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
+)
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 module.exports = environment
