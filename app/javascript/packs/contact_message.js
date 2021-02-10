aaab8f64ca1ee8d4b62ce1bdcd11c5da471c5cec
yuki0920 <ywsep20@gmail.com>
2021-02-11 08:39:37 +0900

    User vue-turbolinks

--- app/javascript/packs/contact_message.js
+++ app/javascript/packs/contact_message.js
@@ -1,5 +1,8 @@
 import Vue from 'vue/dist/vue.esm'
+import TurbolinksAdapter from 'vue-turbolinks'
 import ContactMessage from '../ContactMessage.vue'
+
+Vue.use(TurbolinksAdapter)
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
