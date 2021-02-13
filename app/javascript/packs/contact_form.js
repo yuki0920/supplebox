99aefe14a70aaff8efac090de040ec211e084171
yuki0920 <ywsep20@gmail.com>
2021-02-13 09:36:55 +0900

    Turbolinksをやめた

--- app/javascript/packs/contact_form.js
+++ app/javascript/packs/contact_form.js
@@ -1,17 +1,12 @@
 import Vue from 'vue'
-import TurbolinksAdapter from 'vue-turbolinks'
 import store from '../store/index'
 import ContactForm from '../components/ContactForm'
 
-Vue.use(TurbolinksAdapter)
-
-if (document.getElementById('contact_form')) {
-  document.addEventListener('turbolinks:load', () => {
-    const app = new Vue({
-      el: '#contact_form',
-      components: { ContactForm },
-      store,
-      render: h => h(ContactForm)
-    })
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    el: '#contact_form',
+    components: { ContactForm },
+    store,
+    render: h => h(ContactForm)
   })
-}
+})
