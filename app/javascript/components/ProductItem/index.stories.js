9c07feb5ced07380c853279a4d4925d84b9bbc97
yuki0920 <ywsep20@gmail.com>
2021-02-14 18:28:44 +0900

    Fix rubocop warnings

--- app/javascript/components/ProductItem/index.stories.js
+++ app/javascript/components/ProductItem/index.stories.js
@@ -20,9 +20,9 @@ export const Default = Template.bind({})
 Default.args = {
   product: {
     title: "明治 ザバス ウェイトダウン ヨーグルト風味",
-    picture: "https://images-fe.ssl-images-amazon.com/images/I/51MRScMCrKL.jpg",
+    image_url: "https://images-fe.ssl-images-amazon.com/images/I/51MRScMCrKL.jpg",
     path: "/products/2",
-    brand_name: "SAVAS(ザバス)",
+    brand_amazon_name: "SAVAS(ザバス)",
     post_count: "6",
     rate: "3.4"
   }
