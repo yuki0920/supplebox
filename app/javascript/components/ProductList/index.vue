5bb49e697003b4d502125d17bc2ebe89543f4733
yuki0920 <ywsep20@gmail.com>
2021-02-16 20:22:56 +0900

    ヘッダを追加

--- app/javascript/components/ProductList/index.vue
+++ app/javascript/components/ProductList/index.vue
@@ -1,4 +1,32 @@
 <template>
+  <div class="product-list">
+    <div class="product-list__header">
+      <h2>
+        アイテム一覧
+      </h2>
+    </div>
+    <div class="row justify-content-center">
+      <div class="product-list__form form-inline">
+        <div class="form-group">
+          <input
+            id="q_title_or_brand_amazon_name_cont"
+            class="form-control input-lg"
+            placeholder="ブランドやアイテム名を入力"
+            size="40"
+            type="text"
+            name="q[title_or_brand_amazon_name_cont]"
+          >
+        </div>
+        <input
+          type="submit"
+          name="commit"
+          value="アイテムを検索"
+          class="btn btn-success"
+          data-disable-with="アイテムを検索"
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,4 +43,7 @@ export default {
 </script>
 
 <style scoped>
+.product-list__header {
+  text-align: center;
+}
 </style>
