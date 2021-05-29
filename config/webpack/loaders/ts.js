dcdc781a95fbf9af57ee4e5dc6f527d8bfe475e3
yuki0920 <ywsep20@gmail.com>
2021-05-30 07:51:38 +0900

    不要な設定を除いた

--- config/webpack/loaders/ts.js
+++ config/webpack/loaders/ts.js
@@ -1,11 +1,9 @@
-const PnpWebpackPlugin = require('pnp-webpack-plugin')
-
 module.exports = {
   test: /\.tsx$/,
   loader: "ts-loader",
-  options: PnpWebpackPlugin.tsLoaderOptions({
-    appendTsSuffixTo: [/\.vue$/]
-  }),
+  options: {
+    appendTsSuffixTo: [/\.vue$/],
+  },
   exclude: /node_modules/,
 
 };
