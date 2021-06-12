274fe020ac2ed54eea7a858e029adbf3576368c1
yuki0920 <ywsep20@gmail.com>
2021-06-12 18:59:11 +0900

    Update TopPageComponent

--- app/javascript/components/TopPage/index.vue
+++ app/javascript/components/TopPage/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <ServiceDescription />
+  <div class="p-toppage">
+    <h1 class="p-toppage__title">
+      SuppleBoxとは
+    </h1>
+    <p class="p-toppage__title-annotation">
+      プロテイン選びで失敗したく無い方に向けた、プロテイン特化型の口コミ共有サービスです。
+    </p>
+    <ServiceDescription />
+
+  </div>
 </template>
 
 <script lang="ts">
@@ -17,4 +26,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.p-toppage {
+  &__title {
+
+  }
+  &__title-annotation {
+    font-size: 1.5rem;
+    text-align: center;
+  }
+}
 </style>
