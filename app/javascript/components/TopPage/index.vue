1b5f0d078ed205bd0d0421af186f7a9346a93113
yuki0920 <ywsep20@gmail.com>
2021-06-12 11:25:32 +0900

    Add TopPageComponent

---
+++ app/javascript/components/TopPage/index.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script lang="ts">
+
+export default {
+  setup() {
+    console.log('Hello')
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
