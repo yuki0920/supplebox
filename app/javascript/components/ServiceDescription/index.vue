86ede45559641edef40bc0db90014f654e036eb2
yuki0920 <ywsep20@gmail.com>
2021-06-12 12:19:15 +0900

    Add ServiceDescription Component

---
+++ app/javascript/components/ServiceDescription/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="o-service-description row">
+    <div class="col-md-4 col-sm-6 feature text-center">
+      <i
+        class="o-service-description__icon bi bi-hand-thumbs-up"
+      />
+      <h3 class="feature-title">
+        リアルな口コミ
+      </h3>
+      <p>✔	実際の購入者によるリアルな口コミで失敗しないプロテイン選びをサポート</p>
+    </div>
+    <div class="col-md-4 col-sm-6 feature text-center">
+      <i
+        class="o-service-description__icon bi bi-search"
+      />
+      <h3 class="feature-title">
+        探しやすい
+      </h3>
+      <p>✔	気になるアイテムをお気に入りランキングや最新の口コミから検索可能</p>
+    </div>
+    <div class="col-md-4 col-sm-6 feature text-center">
+      <i
+        class="o-service-description__icon bi bi-card-image"
+      />
+      <h3 class="feature-title">
+        見やすいレイアウト
+      </h3>
+      <p>✔	PCやスマートフォンどちらからでも見やすい設計</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ServiceDescription',
+}
+</script>
+
+<style lang="scss" scoped>
+.o-service-description {
+  &__icon {
+    font-size: 40px;
+  }
+}
+</style>
--- app/javascript/components/TopPage/index.vue
+++ app/javascript/components/TopPage/index.vue
@@ -1,9 +1,13 @@
 <template>
+  <ServiceDescription />
 </template>
 
 <script lang="ts">
-
+import ServiceDescription from "./ServiceDescription/index.vue"
 export default {
+  components: {
+    ServiceDescription
+  },
   setup() {
     console.log('Hello')
     return {
