dd78ee8bb899f854c36fcf54863a3b247bce9799
yuki0920 <ywsep20@gmail.com>
2021-06-15 20:59:59 +0900

    Update Gems for browser test

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,8 +4,4 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
-
-  config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome_headless
-  end
 end
