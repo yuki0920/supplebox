84a10efee41d2dd6007fa91e1defa614ec700fb7
yuki0920 <ywsep20@gmail.com>
2021-06-20 07:43:24 +0900

    Add flash message for create post

--- app/javascript/components/PostForm/index.vue
+++ app/javascript/components/PostForm/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="o-post-form">
+    <FlashMessage
+      :is-display="message.isDisplay"
+      :message-text="message.text"
+      :message-type="message.type"
+    />
     <h2
       class="o-post-form__heading mb-3"
     >
@@ -56,10 +61,11 @@
         <div class="col-md-9">
           <input
             id="picture"
-            @change="setPicture"
+            accept="image/jpeg,image/gif,image/png"
             class="btn"
             type="file"
             name="picture"
+            @change="setPicture"
           >
         </div>
       </div>
@@ -95,10 +101,12 @@
 
 <script lang="ts">
 import axios from '@axios'
+import FlashMessage from '../FlashMessage/index.vue'
 
 import { reactive } from 'vue'
 export default {
   name: 'ContactForm',
+  components: { FlashMessage },
   setup() {
     const formInput = reactive({
         title: '',
@@ -111,23 +119,44 @@ export default {
       e.preventDefault()
       picture = e.target.files[0]
     }
+
+    const message = reactive({
+      isDisplay: false,
+      text: '',
+      type: ''
+    })
+
     const submitForm = async () => {
       const productId = location.pathname.split("/").slice(-1)[0]
       let formData = new FormData()
       formData.append('posts[title]', formInput.title)
       formData.append('posts[rate]', formInput.rate)
+      formData.append('posts[content]', formInput.content)
       formData.append('posts[product_id]', productId)
       formData.append('posts[picture]', picture)
       const config = { headers : { 'content-type': 'multipart/form-data' } }
 
       try {
         await axios.post('/api/posts', formData, config)
+
+        message.isDisplay = true
+        message.text = "口コミを送信しました"
+        message.type = "primary"
+
+        formInput.title = ''
+        formInput.rate = ''
+        formInput.content = ''
+
+        location.reload()
       } catch(error) {
         const { data } = error.response
-        console.log('data', data)
+        message.isDisplay = true
+        message.text = `口コミの送信に失敗しました 「${data.message}」`
+        message.type = "danger"
       }
     }
     return {
+      message,
       formInput,
       setPicture,
       submitForm
