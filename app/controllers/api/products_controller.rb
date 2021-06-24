d5ddb00e90b623e441173943f8c57392b1f02d9d
yuki0920 <ywsep20@gmail.com>
2021-06-25 08:57:42 +0900

    Wip use jb `GET /api/products`

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,16 +3,16 @@
 module Api
   class ProductsController < ApplicationController
     def index
-      products =
+      @products =
         if params[:q].present?
           Product.where(['title LIKE ?', "%#{params[:q]}%"]).or(Product.where(['brand_amazon_name LIKE ?', "%#{params[:q]}%"]))
         else
           Product
         end
 
-      products = products.page(params[:page]).per(params[:per]).includes(:posts, :likes).page(params[:page])
+      @products = @products.page(params[:page]).per(params[:per]).includes(:posts, :likes).page(params[:page])
 
-      render json: products, each_serializer: ProductSerializer
+      render 'index.json.jb'
     end
   end
 end
