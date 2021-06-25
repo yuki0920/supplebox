56c68f1eff52473adf9f2ced0e4f94a51821f908
yuki0920 <ywsep20@gmail.com>
2021-06-26 08:04:32 +0900

    Fix rubocop

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
-class Api::LikesController < ApplicationController
-  before_action :require_user_logged_in, only: %i[create destroy]
+module Api
+  class LikesController < ApplicationController
+    before_action :require_user_logged_in, only: %i[create destroy]
 
-  def create
-    product = Product.find(params[:product_id])
-    current_user.like!(product)
+    def create
+      product = Product.find(params[:product_id])
+      current_user.like!(product)
 
-    render json: { message: 'Product has been liked successfully' }
-  end
+      render json: {message: 'Product has been liked successfully'}
+    end
 
-  def destroy
-    product = Product.find(params[:product_id])
+    def destroy
+      product = Product.find(params[:product_id])
 
-    current_user.unlike!(product)
-    render json: { message: 'Product has been unliked successfully' }
+      current_user.unlike!(product)
+      render json: {message: 'Product has been unliked successfully'}
+    end
   end
 end
