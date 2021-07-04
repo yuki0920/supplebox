0081ac093219fa3e82ef15728d1a4bb9abb304ef
yuki0920 <ywsep20@gmail.com>
2021-07-04 12:29:34 +0900

    Fix nav & footer

--- spec/system/toppages_spec.rb
+++ spec/system/toppages_spec.rb
@@ -14,6 +14,7 @@ describe 'リンク確認', js: true do
       aggregate_failures do
         expect(page).to have_link('プロテイン', href: '/products')
         expect(page).to have_link('口コミ', href: '/posts')
+        expect(page).to have_link('問い合わせ', href: '/contacts')
         expect(page).to have_link('新規登録', href: '/signup')
         expect(page).to have_link('ログイン', href: '/login')
         expect(page).to have_button('テストアカウントでログイン')
@@ -30,8 +31,9 @@ describe 'リンク確認', js: true do
         expect(page).to have_link('利用基準', href: '/policy')
         expect(page).to have_link('プライバシーポリシー', href: '/privacypolicy')
         expect(page).to have_link('お問い合わせ', href: '/contacts')
-        expect(page).to have_link('アイテム一覧を見る', href: '/products')
-        expect(page).to have_link('アイテムを登録する', href: '/products/new')
+        expect(page).to have_link('プロテイン一覧を見る', href: '/products')
+        expect(page).to have_link('口コミ一覧を見る', href: '/posts')
+        expect(page).to have_link('プロテインを登録する', href: '/products/new')
       end
     end
   end
