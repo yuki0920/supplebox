04677c27deead8d065030c8fe95788a548c25a98
yuki0920 <ywsep20@gmail.com>
2021-08-10 23:31:29 +0900

    Add sample api connect

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -50,7 +50,9 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: process.env.API_URL
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,22 +1,29 @@
 <template>
-  <div class="sample">
-    <ul>
-      <li>{{ value }} is example value</li>
-      <li>{{ value }} is example value</li>
-      <li>{{ value }} is example value</li>
-    </ul>
+  <div v-if="product !== null" class="sample">
+    Id {{ product.id }}
+    <br>
+    Title {{ product.title }}
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@nuxtjs/composition-api'
+import { defineComponent, useContext, ref } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup () {
-    const value = ref('sample')
+    // @ts-ignore
+    const { $axios } = useContext()
+    const product = ref(null)
+    const fetchProduct = () => {
+      return $axios.get('/api/products/1')
+    }
+
+    fetchProduct().then(({ data }) => {
+      product.value = data.product
+    })
 
     return {
-      value
+      product
     }
   }
 })
