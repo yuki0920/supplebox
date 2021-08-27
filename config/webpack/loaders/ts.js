8fc5b2152a920667cc49d174c8d353a098ae9374
yuki0920 <ywsep20@gmail.com>
2021-08-28 07:58:09 +0900

    Remove old settings

--- config/webpack/loaders/ts.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  test: /\.tsx$/,
-  loader: "ts-loader",
-  options: {
-    appendTsSuffixTo: [/\.vue$/],
-  },
-  exclude: /node_modules/,
-
-};
--- config/webpack/loaders/vue.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  test: /\.vue$/,
-  use: [{
-    loader: 'vue-loader'
-  }]
-}
